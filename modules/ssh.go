ce9b33f9bd224f122f91516ed753628e3fd8d480
crazylazyowl <58094513+crazylazyowl@users.noreply.github.com>
2023-09-12 16:36:57 +0400

    fix host-key-algorithms

--- modules/ssh.go
+++ modules/ssh.go
@@ -100,8 +100,11 @@ func (s *SSHScanner) Scan(t zgrab2.ScanTarget) (zgrab2.ScanStatus, interface{}, 
 	sshConfig.ConnLog = data
 	sshConfig.ClientVersion = s.config.ClientID
 	sshConfig.HelloOnly = s.config.HelloOnly
-	if err := sshConfig.SetHostKeyAlgorithms(s.config.HostKeyAlgorithms); err != nil {
-		log.Fatal(err)
+	if len(s.config.HostKeyAlgorithms) > 0 {
+		sshConfig.HostKeyAlgorithms = make([]string, 0)
+		if err := sshConfig.SetHostKeyAlgorithms(s.config.HostKeyAlgorithms); err != nil {
+			log.Fatal(err)
+		}
 	}
 	if err := sshConfig.SetKexAlgorithms(s.config.KexAlgorithms); err != nil {
 		log.Fatal(err)
